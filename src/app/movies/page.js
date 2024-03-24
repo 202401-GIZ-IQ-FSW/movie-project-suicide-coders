6f58dfd680ff32a9b7b83d9193bc5a1215d83a61
Mohammed Nazar <x3raqe@gmail.com>
2024-03-24 20:54:56 +0300

    Done with search functionality and logic

--- src/app/movies/page.js
+++ src/app/movies/page.js
@@ -4,21 +4,25 @@ import {
   nowPlayingMovies,
   popularMovies,
   trendingNowMovies,
+  SearchQuery
 } from "@/data/API/mainApi";
 import MoivesQuery from "@/components/QueryData/MoivesQuery";
 import Main from "@/components/landingPage/Main";
+
 
 async function page({ searchParams }) {
   let pageNumber = searchParams.page ? searchParams.page : 1;
   let moviesParam = searchParams.movies;
   let genresParams = searchParams.genre;
-
+  let searchParam = searchParams.search;
   let TrendParams = searchParams.trendTime ? searchParams.trendTime : "day";
+
   let topRatedMoviesData = await topRatedMovies(pageNumber);
   let popularMoviesData = await popularMovies(pageNumber);
   let nowPlayingMoviesData = await nowPlayingMovies(pageNumber);
   let upComingMoviesData = await upComingMovies(pageNumber);
   let trendingNowMoviesData = await trendingNowMovies(TrendParams);
+  let SearchQueryData =  await SearchQuery(searchParam, pageNumber)
   if (genresParams) {
     topRatedMoviesData.results = topRatedMoviesData.results.filter(movie => movie.genre_ids.includes(genresParams-1+1));
     popularMoviesData.results = popularMoviesData.results.filter(movie => movie.genre_ids.includes(genresParams-1+1));
@@ -35,8 +39,9 @@ async function page({ searchParams }) {
    {moviesParam == "Now playing" && <MoivesQuery data={nowPlayingMoviesData} title="Now playing" pageNumber={pageNumber}/>}
    {moviesParam == "Upcoming" && <MoivesQuery data={upComingMoviesData} title="Upcoming" pageNumber={pageNumber}/>}
    {moviesParam == "Trending" && <MoivesQuery data={trendingNowMoviesData} title="Trending" pageNumber={pageNumber}/>}
+   {searchParam && <MoivesQuery data={SearchQueryData} title={`Search " ${searchParam} "`} pageNumber={pageNumber}/>}
 
-   {!moviesParam && <Main
+   {!moviesParam && !searchParam && <Main
         trendingNowMoviesData={trendingNowMoviesData}
         popularMoviesData={popularMoviesData}
         topRatedMoviesData={topRatedMoviesData}
--- src/components/QueryData/MoivesQuery.js
+++ src/components/QueryData/MoivesQuery.js
@@ -12,8 +12,11 @@ const MoivesQuery = ({title, data, pageNumber}) => {
   const genreParams = searchParams.get("genre");
   const moviesParams = searchParams.get("movies");
   const pageParams = searchParams.get("page");
+  const searchParam = searchParams.get("search");
+
+
   const handleClick = ({target}) => {
-    router.push(`/movies?movies=${title}&page=${target.value}${genreParams ? "&genre="+genreParams: ""}`)
+    router.push(`/movies?${searchParam ? "search="+searchParam : "movies="+title }&page=${target.value}${genreParams && !searchParam ? "&genre="+genreParams: ""}`)
   }
 
 
@@ -28,20 +31,20 @@ const MoivesQuery = ({title, data, pageNumber}) => {
         }
         {title != "Trending"&& <div className="join ">
         <button onClick={()=>{
-            pageNumber > 1 ? router.push(`/movies?movies=${title}&page=${pageNumber - 1}${genreParams ? "&genre="+genreParams: ""}`): false;
+            pageNumber > 1 ? router.push(`/movies?${searchParam ? "search="+searchParam : "movies="+title }&page=${pageNumber - 1}${genreParams && !searchParam ? "&genre="+genreParams: ""}`): false;
           }} className="join-item btn md:text-[16px] md:mx-2 text-red-600"><FontAwesomeIcon icon={faBackward}/></button>
           <button onClick={handleClick} value={pageNumber} className="join-item btn md:text-[16px] md:mx-2">{pageNumber}</button>
           <button onClick={()=>{
-            router.push(`/movies?movies=${title}&page=${1}${genreParams ? "&genre="+genreParams: ""}`)
+            router.push(`/movies?${searchParam ? "search="+searchParam : "movies="+title }&page=${1}${genreParams && !searchParam ? "&genre="+genreParams: ""}`)
           }}  className="join-item btn md:text-[16px] md:mx-2">Reset</button>
           <button onClick={handleClick} value={data.total_pages > 1000 ? 500 : data.total_pages} className="join-item btn md:text-[16px] md:mx-2">{data.total_pages > 1000 ? 500 : data.total_pages}</button>
           <button onClick={()=>{
-            pageNumber < 500 ? router.push(`/movies?movies=${title}&page=${pageNumber - 1 + 2}${genreParams ? "&genre="+genreParams: ""}`): false;
+            pageNumber < 500 ? router.push(`/movies?${searchParam ? "search="+searchParam : "movies="+title }&page=${pageNumber - 1 + 2}${genreParams && !searchParam ? "&genre="+genreParams: ""}`): false;
           }} className="join-item btn md:text-[16px] md:mx-2 text-red-600"><FontAwesomeIcon icon={faForward}/></button>
         </div>}
 
       </div>
-      <div className="flex flex-wrap gap-5 items-center py-5 justify-center">
+      <div className="flex flex-wrap gap-2 items-center py-5 justify-evenly">
       {data?.results.map((movie,i)=>
       <MovieCard key={i} moiveData={movie}/>
       )}
