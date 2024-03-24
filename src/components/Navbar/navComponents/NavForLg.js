e15b565cb7378544a484e7d1bb86c36b2af5eaaa
Mohammed Nazar <x3raqe@gmail.com>
2024-03-24 22:39:46 +0300

    Upate card style

--- src/components/Navbar/navComponents/NavForLg.js
+++ src/components/Navbar/navComponents/NavForLg.js
@@ -5,17 +5,21 @@ import Movies from '../dropDownBtn/Movies'
 import Actors from './Actors'
 import ThemeSwitcher from '@/components/Theme/ThemeSwitcher'
 import SearchInputLg from './SearchInputLg'
+import TvShows from './TvShows'
 
 // Nav for lg screens 
 const NavForLg = ({genresListData}) => {
   return (
     
-        <ul className=' lg:gap-x-14 md:gap-x-4  grid-cols-5 hidden md:flex '>
+        <ul className=' lg:gap-x-10 md:gap-x-4  grid-cols-5 hidden md:flex '>
             <li>
                 <Genres genresListData={genresListData} />
             </li>
             <li>
                 <Movies />
+            </li>
+            <li>
+                <TvShows/>
             </li>
             <li>
                 <Actors/>
--- src/data/API/movieDetailsApi.js
+++ src/data/API/movieDetailsApi.js
@@ -18,9 +18,24 @@ export async function movieDetails(id){
     return data;
 }
 
+export async function MovieSimilar(id){
+  const res = await fetch(`https://api.themoviedb.org/3/movie/${id}/similar?language=en-US&page=1`, options);
+  const data = await res.json();
+  return data;
+}
+
+export async function MovieVideos(id){
+  const res = await fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)  ;
+  const data = await res.json();
+  return data;
+}
+
+
+
 
 export async function Reviews(){
   const res = await fetch('https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1', options);
   const data = await res.json();
   return data;
 }
+
