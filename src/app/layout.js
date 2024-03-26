4083296afe26413ac6c522289166602bf8e4458f
Mohammed Nazar <x3raqe@gmail.com>
2024-03-26 12:08:51 +0300

    added favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,13 +6,15 @@ import Footer from "@/components/Footer/Footer";
 import Provider from "@/components/Theme/Provider";
 
 const inter = Inter({ subsets: ["latin"] });
-import {Suspense} from "react";
-
+import { Suspense } from "react";
 
 export const metadata = {
   title: "Movie app",
   description:
-    "App Name is the world's most popular and authoritative source for movie, TV and celebrity content. Find ratings and reviews for the newest movie and TV shows.",
+    "Pluto is the world's most popular and authoritative source for movie, TV and celebrity content. Find ratings and reviews for the newest movie and TV shows.",
+    icons: {
+      icon: "https://media.istockphoto.com/id/1306098836/vector/graceful-floral-p-letter-icon-design.jpg?s=612x612&w=0&k=20&c=BxjYIFGFVcWI0ZlSmW_rKj827n7QZCsk9agCAY-PJsc=",
+    },
 };
 
 export default function RootLayout({ children }) {
@@ -20,10 +22,16 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Provider>
-        <Suspense fallback={<div className="h-screen w-screen flex justify-center items-center text-6xl"><span className="loading loading-infinity loading-lg"></span></div>}>
-          <Navbar />
-          {children}
-          <Footer />
+          <Suspense
+            fallback={
+              <div className="h-screen w-screen flex justify-center items-center text-6xl">
+                <span className="loading loading-infinity loading-lg"></span>
+              </div>
+            }
+          >
+            <Navbar />
+            {children}
+            <Footer />
           </Suspense>
         </Provider>
       </body>
